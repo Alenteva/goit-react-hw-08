04e30839f103977f9d640c47888637a193f3173a
Alenteva <144173680+Alenteva@users.noreply.github.com>
2024-05-06 14:34:19 +0300

    Update ContactsPage.jsx

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
-import ContactForm from "../../components/ContactForm/ContactForm";
-import ContactList from "../../components/ContactList/ContactList";
-import SearchBox from "../../components/SearchBox/SearchBox";
+import ContactForm from "../../components/contactform/contactform";
+import ContactList from "../../components/contactList/contactList";
+import SearchBox from "../../components/searchBox/searchBox";
 import { useDispatch } from "react-redux";
 import { fetchContacts } from "../../redux/contacts/operations";
 import { Toaster } from "react-hot-toast";
